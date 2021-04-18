0e0d67933999aac8b68a70a7bf923e9bb329ba54
nrafdilapebriyani <nrafdilafebri29@gmailcom>
2021-04-18 08:33:41 +0700

    excercise

---
+++ app/src/main/java/com/example/excercise01/ClassNama.java
@@ -0,0 +1,13 @@
+package com.example.excercise01;
+
+public class ClassNama {
+    private String Nama;
+
+    public ClassNama(String nama){
+        this.Nama = nama;
+    }
+
+    public String getNama(){
+        return this.Nama;
+    }
+}
